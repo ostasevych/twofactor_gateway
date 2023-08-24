f138f04e80610e2b2014b37c33fad89ec0a0bcfd
Oleksa <ostasevych@users.noreply.github.com>
2023-08-24 14:07:41 +0300

    Update challenge.php
    Redone template to make the style similar to the twofactor_totp app, in particular, added icon of a device, and the Submit button.
    
    Signed-off-by: Oleksa <ostasevych@users.noreply.github.com>

--- templates/challenge.php
+++ templates/challenge.php
@@ -2,16 +2,17 @@
 style('twofactor_gateway', 'style');
 ?>
 
-<form method="POST" class="sms-2fa-form">
-	<input type="text"
-		   class="challenge"
-		   name="challenge"
-		   required="required"
-		   autofocus
-		   autocomplete="off"
-		   autocapitalize="off"
-		   value="<?php echo isset($_['secret']) ? $_['secret'] : '' ?>"
-		   placeholder="<?php p($l->t('Authentication code')) ?>">
-	<input type="submit" class="confirm-inline icon-confirm" value="">
-	<p><?php p($l->t('An access code has been sent to %s', [$_['phone']])); ?></p>
+<img class="two-factor-icon two-factor-gateway-icon" src="<?php print_unescaped(image_path('twofactor_gateway', 'app.svg')); ?>" alt="">
+
+<p><?php p($l->t('An access code has been sent to %s', [$_['phone']])); ?></p>
+
+
+<form method="POST" class="sms2-fa-form">
+        <input type="tel" minlength="6" maxlength="10" name="challenge" required="required" autofocus autocomplete="off" autocapitalize="off" placeholder="<?php p($l->t('Authentication code')) ?>">
+        <button
+                class="primary two-factor-submit"
+                type="submit"
+                value="<?php echo isset($_['secret']) ? $_['secret'] : '' ?>">
+                <?php p($l->t('Submit')); ?>
+        </button>
 </form>
